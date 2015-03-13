dc353d10605f990ba746fc9e5d73c0d9a70c965d
Santiago Palladino <spalladino@gmail.com>
2015-03-13 18:46:56 -0300

    Specify files to import via command line

--- src/importer/main.go
+++ src/importer/main.go
@@ -4,16 +4,96 @@ import (
 	"load"
 	"log"
 	"store"
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
 )
 
 func main() {
-	db, err := store.NewSqlStore()
-	// db, err := store.NewElasticSearchStore()
-	if err != nil {
-		log.Fatal(err)
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s [options] FILES:\n", os.Args[0])
+		flag.PrintDefaults()
 	}
 
-	load.LoadShapefile(db, "/Users/waj/Downloads/ARG_adm/ARG_adm0.shp", "gadm", []string{"ISO"}, "NAME_ENGLI")
-	load.LoadShapefile(db, "/Users/waj/Downloads/ARG_adm/ARG_adm1.shp", "gadm", []string{"ISO", "ID_1"}, "NAME_1")
-	load.LoadShapefile(db, "/Users/waj/Downloads/ARG_adm/ARG_adm2.shp", "gadm", []string{"ISO", "ID_1", "ID_2"}, "NAME_2")
+	var source, id, storetype string
+
+	flag.StringVar(&source, "source", "", "Data source (required). Currently supports gadm and naturalearth.")
+	flag.StringVar(&id, "id", "", "Identifier prefix for all imported data, defaults to shortened source name.")
+	flag.StringVar(&storetype, "store", "sql", "Store type to use. Available: sql, elasticsearch.")
+
+	flag.Parse()
+
+	if (source == "") {
+		fmt.Fprintf(os.Stderr, "Error: Option 'source' is required\n")
+		flag.Usage()
+		return
+	}
+
+	var db store.Store
+	var err error
+	switch storetype {
+		case "sql", "psql":
+			db, err = store.NewSqlStore()
+		case "elastic", "es", "elasticsearch":
+			db, err = store.NewElasticSearchStore()
+	}
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	for _, file := range flag.Args() {
+		var defaultId, nameField string
+		var idFields []string
+		loadSourceSettings(file, source, &defaultId, &idFields, &nameField)
+		if (id == "") {
+			id = defaultId
+		}
+
+		fmt.Printf("\nProcessing %s file %s:\n ID prefix: %s\n ID fields: %s\n Name field: %s\n\n", source, file, id, idFields, nameField)
+		load.LoadShapefile(db, file, id, idFields, nameField)
+	}
+}
+
+func loadSourceSettings(file string, source string, defaultId *string, idFields *[]string, nameField *string) {
+	switch (source) {
+
+	case "gadm", "GADM":
+		*defaultId = "gadm"
+		gadmRegexp := regexp.MustCompile("(?i)^(?:.+/)?[a-z]+_adm(\\d+)\\.shp$")
+		match := gadmRegexp.FindStringSubmatch(file)
+		level, _ := strconv.Atoi(match[1])
+
+		if (level == 0) {
+			*idFields = []string{"ISO"}
+			*nameField = "NAME_ENGLI"
+		} else {
+			*idFields = make([]string, level+1)
+			(*idFields)[0] = "ISO"
+			for i := 1; i <= level; i++ {
+				(*idFields)[i] = "ID_" + strconv.Itoa(i)
+			}
+			*nameField = "NAME_" + strconv.Itoa(level)
+		}
+
+	case "naturalearth", "NATURALEARTH", "ne", "NE":
+		*defaultId = "ne"
+		neRegexp := regexp.MustCompile("(?i)^(?:.+/)?[a-z0-9_]+_admin_(\\d+)_[a-z0-9_]+\\.shp$")
+		match := neRegexp.FindStringSubmatch(file)
+		level, _ := strconv.Atoi(match[1])
+
+		if (level == 0) {
+			*idFields = []string{"SOV_A3"}
+			*nameField = "ADMIN"
+		} else if (level == 1) {
+			*idFields = []string{"sov_a3", "diss_me"}
+			*nameField = "name"
+		} else {
+			panic("Level " + match[1] + " not supported for natural earth data")
+		}
+
+	}
 }
